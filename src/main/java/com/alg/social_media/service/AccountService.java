64830eb3151035f756776c669aafececaffe372c
Alexander <alexandergerontas@gmail.com>
2024-01-10 01:49:33 +0200

    remove exception if user not found

--- src/main/java/com/alg/social_media/service/AccountService.java
+++ src/main/java/com/alg/social_media/service/AccountService.java
@@ -1,7 +1,6 @@
 package com.alg.social_media.service;
 
 import com.alg.social_media.converters.AccountConverter;
-import com.alg.social_media.exceptions.AccountDoesNotExistException;
 import com.alg.social_media.exceptions.AccountExistsException;
 import com.alg.social_media.exceptions.GenericError;
 import com.alg.social_media.objects.Account;
@@ -38,17 +37,7 @@ public class AccountService {
         return newAccount;
     }
 
-    public Account findByUsername(String username)
-        throws AccountExistsException, AccountDoesNotExistException {
-
-        // search for account with the same username
-        var account = accountRepository.findByUsername(username);
-
-        // if an account does not exist throw exception
-        if (account == null) {
-            throw new AccountDoesNotExistException(GenericError.ACCOUNT_DOES_NOT_EXIST, username);
-        }
-
-        return account;
+    public Account findByUsername(String username) {
+      return accountRepository.findByUsername(username);
     }
 }
