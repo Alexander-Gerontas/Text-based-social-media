4f216f5ca42964004f89636f6943e53345ad4c2a
Alexander <alexandergerontas@gmail.com>
2024-01-10 15:21:44 +0200

    updated exceptions

--- src/main/java/com/alg/social_media/handler/GlobalControllerExceptionHandler.java
+++ src/main/java/com/alg/social_media/handler/GlobalControllerExceptionHandler.java
@@ -4,11 +4,14 @@ import com.alg.social_media.exceptions.AccountDoesNotExistException;
 import com.alg.social_media.exceptions.AccountExistsException;
 import com.alg.social_media.exceptions.InvalidTokenException;
 import com.alg.social_media.exceptions.NoTokenProvidedException;
+import com.alg.social_media.exceptions.PostException;
+import com.alg.social_media.exceptions.SubscriptionException;
 import com.alg.social_media.exceptions.WrongPasswordException;
 import io.javalin.http.ExceptionHandler;
 import io.javalin.http.HttpStatus;
 
 public class GlobalControllerExceptionHandler {
+  // Authentication Exceptions
   public static final ExceptionHandler<AccountExistsException> handleAccountExists = (e, ctx) ->
       ctx.status(HttpStatus.BAD_REQUEST).result(e.getMessage());
 
@@ -24,6 +27,14 @@ public class GlobalControllerExceptionHandler {
   public static final ExceptionHandler<InvalidTokenException> handleInvalidTokenProvided = (e, ctx) ->
       ctx.status(HttpStatus.UNAUTHORIZED).result(e.getMessage());
 
+  // Subscription Exceptions
+  public static final ExceptionHandler<SubscriptionException> handleSubscriptionException = (e, ctx) ->
+      ctx.status(HttpStatus.BAD_REQUEST).result(e.getMessage());
+
+  public static final ExceptionHandler<PostException> handlePostException = (e, ctx) ->
+      ctx.status(HttpStatus.BAD_REQUEST).result(e.getMessage());
+
+  // Generic Exceptions
   public static final ExceptionHandler<Exception> exceptionHandler = (e, ctx) ->
       ctx.status(HttpStatus.BAD_REQUEST).result(e.getMessage());
 
