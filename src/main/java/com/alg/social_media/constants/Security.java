3518dc0c85b62256115584124bdc4e89da415514
Alexander <alexandergerontas@gmail.com>
2024-01-29 17:36:16 +0200

    remove unused const

--- src/main/java/com/alg/social_media/constants/Security.java
+++ src/main/java/com/alg/social_media/constants/Security.java
@@ -6,7 +6,6 @@ public final class Security {
 
   private Security() {}
 
-  public static final String PASSWORD_SALT = "saltandpepper";
   public static final String JWT_SECRET_KEY = "7e8a003c2bf1348846222330d3458712ad9b1de6a1ec8bcf9d41e6ecc66bea2f";
   public static final long JWT_EXPIRATION_TIME = Duration.ofHours(2).toMillis();
 }
